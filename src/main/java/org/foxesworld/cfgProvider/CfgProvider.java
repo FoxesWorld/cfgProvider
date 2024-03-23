e07a67c3a7b4b7388c5d268bb5942f6a1ecd219d
Aiden <miomoor182@gmail.com>
2024-03-23 12:52:35 +0300

    2.0.1-STABLE

--- src/main/java/org/foxesworld/cfgProvider/CfgProvider.java
+++ src/main/java/org/foxesworld/cfgProvider/CfgProvider.java
@@ -2,64 +2,69 @@ package org.foxesworld.cfgProvider;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Calendar;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-/**
- *
- * @author AidenFox
- */
 public class CfgProvider {
 
+    private static final Map<String, Object> defaultConfig;
 
-    /*ROOT cfg*/
-    private final static Map<String, Object> defaultConfig = readJsonCfg(CfgProvider.class.getClassLoader().getResourceAsStream("assets/cfg/defaultCfg.json"));
-    
-        private static String baseDirPath = getWorkdir((Integer) defaultConfig.get("baseDirIndex"));
-        private static String homeDirName = (String) defaultConfig.get("homeDir");
-        private static String currentCfg;
-        private static String defaultConfFilesDir = (String) defaultConfig.get("tplBaseDir");
-        private static String cfgFileExtension = (String) defaultConfig.get("cfgExtension");
-        private static String cfgExportDirName = (String) defaultConfig.get("cfgExportDir");
-        private static Boolean debug = (Boolean) defaultConfig.get("debug");
+    private static String baseDirPath;
+    private static String homeDirName;
+    private static String currentCfg;
+    private static String defaultConfFilesDir;
+    private static String cfgFileExtension;
+    private static String cfgExportDirName;
+    private static Boolean debug;
 
-    /*ENVIRONMENT PATHs*/
-    private final static String GAMEFULLPATH = baseDirPath + File.separator + CfgProvider.homeDirName + File.separator;
+    private static final String GAMEFULLPATH;
     private static String readNote;
-    private static final Integer MONTH = Calendar.getInstance().get(Calendar.MONTH) + 1;
+    private static final Integer MONTH;
 
-    /*OUTPUT*/
-    private static Map<String, Object> configLines = new HashMap<>();
-    private static Map<String, Map> cfgMaps = new HashMap<>();
+    private static final Map<String, Object> configLines = new HashMap<>();
+    private static final Map<String, Map<String, Object>> cfgMaps = new HashMap<>();
+
+    static {
+        defaultConfig = readJsonCfg(CfgProvider.class.getClassLoader().getResourceAsStream("assets/cfg/defaultCfg.json"));
+        baseDirPath = getWorkdir((Integer) defaultConfig.get("baseDirIndex"));
+        homeDirName = (String) defaultConfig.get("homeDir");
+        defaultConfFilesDir = (String) defaultConfig.get("tplBaseDir");
+        cfgFileExtension = (String) defaultConfig.get("cfgExtension");
+        cfgExportDirName = (String) defaultConfig.get("cfgExportDir");
+        debug = (Boolean) defaultConfig.get("debug");
+
+        GAMEFULLPATH = baseDirPath + File.separator + homeDirName;
+        MONTH = Calendar.getInstance().get(Calendar.MONTH) + 1;
+    }
 
     public CfgProvider(String template) {
-        String inputCfgPath = CfgProvider.defaultConfFilesDir + template;
+        String inputCfgPath = defaultConfFilesDir + template;
         Map<String, Object> cfgFileContents = readJsonCfg(CfgProvider.class.getClassLoader().getResourceAsStream(inputCfgPath));
         currentCfg = template.split("\\.")[0];
         String absoluteCfgPath = getFileAbsolutePath(currentCfg);
-        
-        if(!template.contains("internal")){
+
+        if (!template.contains("internal")) {
             File absoluteFileCfgPath = new File(absoluteCfgPath);
-            if(absoluteFileCfgPath.exists()) {
-                 readNote = "    - Reading `" + currentCfg + "` from external storage " + absoluteFileCfgPath;
+            if (absoluteFileCfgPath.exists()) {
+                readNote = "    - Reading `" + currentCfg + "` from external storage " + absoluteFileCfgPath;
             } else {
-                 readNote = "    - Creating `" + absoluteCfgPath + "` from inputStream " + absoluteFileCfgPath;
-                 JsonWriter jsonWriter = new JsonWriter(new File(absoluteCfgPath), cfgFileContents); 
+                readNote = "    - Creating `" + absoluteCfgPath + "` from inputStream " + absoluteFileCfgPath;
+                JsonWriter jsonWriter = new JsonWriter(new File(absoluteCfgPath), cfgFileContents);
             }
             setConfigLines(readJsonCfg(new File(absoluteCfgPath)));
         } else {
             readNote = "    - Reading `" + currentCfg + "` from inputStream " + inputCfgPath;
             setConfigLines(cfgFileContents);
         }
-        
-            if ("true".equals(debug)) {
-                System.out.println(readNote);
-            }
+
+        if (debug) {
+            System.out.println(readNote);
+        }
         putCfgMap();
     }
 
@@ -67,103 +72,108 @@ public class CfgProvider {
         String path;
         switch (index) {
             case 1:
-                //In user's HOMEDIR
                 path = System.getProperty("user.home", "");
                 break;
-
             case 2:
-                //On user's SYSTEM-DRIVE
                 path = System.getenv("SYSTEMDRIVE");
                 break;
-
+            case 3:
+                path = System.getenv("APPDATA");
+                break;
             default:
-                //In a folder launched from
                 path = "";
                 break;
-
         }
         return path;
     }
 
-    protected static HashMap<String, Object> readJsonCfg(InputStream is) {
-        HashMap<String, Object> map = null;
+    protected static Map<String, Object> readJsonCfg(InputStream is) {
+        Map<String, Object> map = null;
         try {
             ObjectMapper mapper = new ObjectMapper();
-            TypeReference<HashMap<String, Object>> typeRef = new TypeReference<>() {
-            };
+            TypeReference<HashMap<String, Object>> typeRef = new TypeReference<HashMap<String, Object>>() {};
             map = mapper.readValue(is, typeRef);
-        } catch (IOException ignored) {}
-
+        } catch (IOException ignored) {
+        }
         return map;
     }
 
-    protected static HashMap<String, Object> readJsonCfg(File path) {
-        HashMap<String, Object> map = null;
-        ObjectMapper mapper = new ObjectMapper();
-        TypeReference<HashMap<String, Object>> typeRef = new TypeReference<>() {
-        };
+    protected static Map<String, Object> readJsonCfg(File path) {
+        Map<String, Object> map = null;
         try {
+            ObjectMapper mapper = new ObjectMapper();
+            TypeReference<HashMap<String, Object>> typeRef = new TypeReference<HashMap<String, Object>>() {};
             map = mapper.readValue(path, typeRef);
-        } catch (IOException ex) {}
-
+        } catch (IOException ex) {
+        }
         return map;
     }
-    
+
     private static String getFileAbsolutePath(String cfgName) {
         return GAMEFULLPATH + File.separator + cfgExportDirName + File.separator + cfgName + cfgFileExtension;
     }
-    
+
     private static void putCfgMap() {
         cfgMaps.put(getCurrentCfgName(), getConfigLines());
     }
-    
-    private static void setConfigLines(Map configLines){
-        CfgProvider.configLines = configLines;
+
+    private static void setConfigLines(Map<String, Object> configLines) {
+        CfgProvider.configLines.clear();
+        CfgProvider.configLines.putAll(configLines);
     }
+
     @SuppressWarnings("unused")
-    public static void setHomeDir(String homeDir){
-        CfgProvider.homeDirName = homeDir;
+    public static void setHomeDir(String homeDir) {
+        homeDirName = homeDir;
     }
+
     @SuppressWarnings("unused")
-    public static void setBaseDirPathIndex(int index){
-        CfgProvider.baseDirPath = getWorkdir(index);
+    public static void setBaseDirPathIndex(int index) {
+        baseDirPath = getWorkdir(index);
     }
+
     @SuppressWarnings("unused")
     public static void setDefaultConfFilesDir(String directory) {
-        CfgProvider.defaultConfFilesDir = directory;
+        defaultConfFilesDir = directory;
     }
+
     @SuppressWarnings("unused")
     public static void setCfgFileExtension(String extension) {
-        CfgProvider.cfgFileExtension = extension;
+        cfgFileExtension = extension;
     }
+
     @SuppressWarnings("unused")
     public static void setCfgExportDirName(String dirName) {
-        CfgProvider.cfgExportDirName = dirName;
+        cfgExportDirName = dirName;
     }
+
     @SuppressWarnings("unused")
-    public static void setDebug(Boolean debug){
+    public static void setDebug(Boolean debug) {
         CfgProvider.debug = debug;
     }
-    
-    public static String getReadNote(){
+
+    public static String getReadNote() {
         return readNote;
     }
-    
+
     public static String getCurrentCfgName() {
         return currentCfg;
     }
-    
-    public static Map getConfigLines() {
-        return configLines;
+
+    public static Map<String, Object> getConfigLines() {
+        return new HashMap<>(configLines);
     }
+
     @SuppressWarnings("unused")
-    public static Map<String, Map> getAllCfgMaps() {
-        return cfgMaps;
+    public static Map<String, Map<String, Object>> getAllCfgMaps() {
+        return new HashMap<>(cfgMaps);
     }
-    
-    public static HashMap<String, List<Object>> getCfgMap(String mapName) {
-        return (HashMap<String, List<Object>>) cfgMaps.get(mapName);
+
+    @SuppressWarnings("unchecked")
+    public static Map getCfgMap(String mapName) {
+        return cfgMaps.get(mapName);
     }
+
     @SuppressWarnings("unused")
     public static Integer getMonth() {
         return MONTH;
--- src/main/java/org/foxesworld/cfgProvider/Test.java
+++ src/main/java/org/foxesworld/cfgProvider/Test.java
@@ -1,29 +1,66 @@
 package org.foxesworld.cfgProvider;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Map;
 
 /**
+ * Test class with GUI.
+ * It displays a frame with a button to trigger an action and a window to show messages.
+ *
  * @author AidenFox
  */
 public class Test {
 
     private static CfgProvider test;
-    private static Map testMap = null;
+    private static Map<String, Object> testMap = null;
 
     public static void main(String[] args) {
-        new CfgProvider("test.json");
+        test = new CfgProvider("test.json");
         testMap = CfgProvider.getCfgMap(CfgProvider.getCurrentCfgName());
-        mapList();
-    }
-    
-    private static void mapList() {
-        Object inputArray;
-         for (Object inputArrName : testMap.keySet()) {
-           inputArray = testMap.get(inputArrName);
-             System.out.println(testMap.get(inputArrName));
-         }
-         System.out.println(CfgProvider.getReadNote());
+
+        SwingUtilities.invokeLater(() -> createAndShowGUI());
     }
 
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("Test Application");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel(new BorderLayout());
+        JButton button = new JButton("Trigger Action");
+
+        JTextArea textArea = new JTextArea();
+        textArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        button.addActionListener(e -> displayMapContents(textArea));
+
+        panel.add(button, BorderLayout.NORTH);
+        panel.add(scrollPane, BorderLayout.CENTER);
+
+        // Add label to display read note
+        JLabel readNoteLabel = new JLabel();
+        panel.add(readNoteLabel, BorderLayout.SOUTH);
+
+        frame.getContentPane().add(panel);
+        frame.setPreferredSize(new Dimension(400, 300));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    private static void displayMapContents(JTextArea textArea) {
+        StringBuilder message = new StringBuilder();
+        for (String key : testMap.keySet()) {
+            Object value = testMap.get(key);
+            message.append(key).append(": ").append(value).append("\n");
+        }
+        textArea.setText(message.toString());
+
+        // Display read note
+        String readNote = CfgProvider.getReadNote();
+        JOptionPane.showMessageDialog(null, readNote, "Read Note", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
